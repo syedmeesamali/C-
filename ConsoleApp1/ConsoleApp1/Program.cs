2931a21c22189df72e4fd988199628dc7371974e
syedmeesamali <syedmeesamali@yahoo.com>
2019-11-24 23:33:26 +0400

    csharp starting out

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a; double b, c;
+            a = 20;
+            b = 13.5;
+            c = a + b;
+            Console.WriteLine("sum is = " + c);
+            Console.WriteLine(int.MaxValue);
+
+        }
+    }
+}
