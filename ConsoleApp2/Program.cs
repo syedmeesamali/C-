86bfe03f8f2e30af6e709639d0c285dff7eb2712
syedmeesamali <syedmeesamali@yahoo.com>
2019-11-25 01:33:52 +0400

    simple gui app done

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "syed";
+            int age = 34;
+            DateTime birthDate;
+            birthDate = new DateTime(1986, 6, 1);
+            Console.WriteLine(firstName);
+            Console.WriteLine(age);
+            Console.WriteLine(birthDate);
+        }
+    }
+}
