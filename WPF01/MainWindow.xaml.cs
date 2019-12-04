3ec8d505cc8d779d9650c71a67fa74f339d41268
syedmeesamali <syedmeesamali@yahoo.com>
2019-12-04 09:01:49 +0400

    latest

--- WPF01/MainWindow.xaml.cs
+++ WPF01/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPF01
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
