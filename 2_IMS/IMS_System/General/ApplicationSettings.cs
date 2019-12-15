4e0bf9c09bf65a6ede3336e58eeaebbb0430b33c
syedmeesamali <syedmeesamali@yahoo.com>
2019-12-15 23:57:44 +0400

    Connection string and config manager settings

---
+++ 2_IMS/IMS_System/General/ApplicationSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace IMS_System.General
+{
+    public class ApplicationSettings
+    {
+        public static string ConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["rbx"].ConnectionString;
+        }
+    }
+}
