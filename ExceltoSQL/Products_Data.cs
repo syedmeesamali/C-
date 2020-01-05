bb515c467db1cf6242c2fe9142340d4c143a9604
syedmeesamali <syedmeesamali@yahoo.com>
2020-01-05 23:55:35 +0400

    SQL connection for my software

---
+++ ExceltoSQL/Products_Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceltoSQL
+{
+    public class Products_Data
+    {
+        public string product_Code { set; get; }
+        public string product_Name { set; get; }
+    }
+}
