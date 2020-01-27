83aa3714cdb94dc5c71e34c6a7b05052d9387aa0
syedmeesamali <syedmeesamali@yahoo.com>
2020-01-27 23:33:21 +0400

    Working perfectly at Home pc as well - neeed packaging related issue update now

---
+++ 8b_Work_Log_Home/Form2.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Work_Log
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.MasterDBDataSet = new Work_Log.MasterDBDataSet();
+            this.ProductsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ProductsTableAdapter = new Work_Log.MasterDBDataSetTableAdapters.ProductsTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.MasterDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource2.Name = "DataSet1";
+            reportDataSource2.Value = this.ProductsBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Work_Log.ProductsData.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(878, 499);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // MasterDBDataSet
+            // 
+            this.MasterDBDataSet.DataSetName = "MasterDBDataSet";
+            this.MasterDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // ProductsBindingSource
+            // 
+            this.ProductsBindingSource.DataMember = "Products";
+            this.ProductsBindingSource.DataSource = this.MasterDBDataSet;
+            // 
+            // ProductsTableAdapter
+            // 
+            this.ProductsTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 499);
+            this.Controls.Add(this.reportViewer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "Report Printout Form";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MasterDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource ProductsBindingSource;
+        private MasterDBDataSet MasterDBDataSet;
+        private MasterDBDataSetTableAdapters.ProductsTableAdapter ProductsTableAdapter;
+    }
+}
