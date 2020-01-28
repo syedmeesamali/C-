b0da40f6cc1aafe2dea5300d62c4bbeb29b201b8
syedmeesamali <syedmeesamali@yahoo.com>
2020-01-28 13:10:39 +0400

    Scaffolding for new IMS system
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

---
+++ 9_IMS_Input/IMS_Input/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMS_Input
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 aboutBox1 = new AboutBox1();
+            aboutBox1.Show();
+        }
+    }
+}
