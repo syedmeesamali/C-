a4dad0aa6071033e1afdc48c23b6aa4339ff64c2
syedmeesamali <syedmeesamali@yahoo.com>
2020-02-10 23:00:16 +0400

    all stuff within try-catch and some refactoring of data input

--- 10_IMS_Final/Form1.cs
+++ 10_IMS_Final/Form1.cs
@@ -15,6 +15,8 @@ namespace IMS_Final
 
         //DataTableCollection tableCollection;
         DataTable tbl = new DataTable("Details");
+        
+        
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -34,42 +36,53 @@ namespace IMS_Final
             {
                 if (openFileDialog.ShowDialog() == DialogResult.OK) //If result is OK
                 {
-
-                    //string fileName;
-                    FileInfo fileName = new FileInfo("" + openFileDialog.FileName);
-                    ExcelPackage package = new ExcelPackage(fileName);
-                    MessageBox.Show("FileName: " + fileName.ToString());
-                    ExcelWorksheet ws = package.Workbook.Worksheets[1];
-                    
-                    int col = 1;
-                    tbl.Columns.Add("Prod ID", typeof(String));
-                    tbl.Columns.Add("Description", typeof(String));
-                    tbl.Columns.Add("Boxes", typeof(String));
-                    tbl.Columns.Add("Pcs", typeof(String));
-                    tbl.Columns.Add("Price", typeof(String));
-                    tbl.Columns.Add("Units", typeof(String));
-                    
-                    int[] offsets = {1, 3, 6, 9, 11, 16}; // use for offsets of columns
-                    
-                    for (int row = 15; row < 30; row++)
+                    int counter = 0;
+                    try
                     {
-                        DataRow dr = tbl.NewRow();
-                        if (ws.Cells[row, col].Value != null)
+                        //string fileName;
+                        FileInfo fileName = new FileInfo("" + openFileDialog.FileName);
+                        ExcelPackage package = new ExcelPackage(fileName);
+                        MessageBox.Show("FileName: " + fileName.ToString());
+                        ExcelWorksheet ws = package.Workbook.Worksheets[1];
+                        counter++;
+                        int col = 1;
+                        if (counter == 1) //Only for the first time define table structure
                         {
-                            dr[0] = ws.Cells[row, col].Value;
-                            dr[1] = ws.Cells[row, col + 3].Value;
-                            dr[2] = ws.Cells[row, col + 6].Value;
-                            dr[3] = ws.Cells[row, col + 9].Value;
-                            dr[4] = ws.Cells[row, col + 11].Value;
-                            dr[5] = ws.Cells[row, col + 15].Value;
-                            tbl.Rows.Add(dr); //Add the prepared row to table
-                        }
-                    } //End of for loop to input Excel data
-                    dataGridView1.AutoGenerateColumns = true;
-                    dataGridView1.Columns.Clear();
-                    dataGridView1.DataSource = tbl;
-                    dataGridView1.Refresh();
+                            tbl.Columns.Add("Prod ID", typeof(String));
+                            tbl.Columns.Add("Description", typeof(String));
+                            tbl.Columns.Add("Boxes", typeof(String));
+                            tbl.Columns.Add("Pcs", typeof(String));
+                            tbl.Columns.Add("Price", typeof(String));
+                            tbl.Columns.Add("Units", typeof(String));
+                        } else
+                        {
+                            for (int row = 15; row < 30; row++)
+                            {
+                                DataRow dr = tbl.NewRow();
+                                if (ws.Cells[row, col].Value != null)
+                                {
+                                    //Populate the columns and rows of our defined datatable
+                                    dr[0] = ws.Cells[row, col].Value;
+                                    dr[1] = ws.Cells[row, col + 3].Value;
+                                    dr[2] = ws.Cells[row, col + 6].Value;
+                                    dr[3] = ws.Cells[row, col + 9].Value;
+                                    dr[4] = ws.Cells[row, col + 11].Value;
+                                    dr[5] = ws.Cells[row, col + 15].Value;
+                                    tbl.Rows.Add(dr); //Add the prepared row to table
+                                }
+                            } //End of for loop to input Excel data
+                            dataGridView1.AutoGenerateColumns = true;
+                            dataGridView1.Columns.Clear();
+                            dataGridView1.DataSource = tbl;
+                            dataGridView1.Refresh();
+                        } //End of else block
+                        
+                    } //End of try block
 
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
                 } //End of dialog selection
             }//End of filter
         }
