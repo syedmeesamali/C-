a7cf2cd227c744520c3b0456446a7f9f2dc9a725
syedmeesamali <syedmeesamali@yahoo.com>
2020-02-13 12:53:22 +0400

    RDLC added to my repo so i can always access when required - Very import for winform reports and reportviewer
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 10_IMS_Final/StockReportsForm.Designer.cs
+++ 10_IMS_Final/StockReportsForm.Designer.cs
@@ -39,6 +39,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -72,7 +73,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(231, 34);
             this.button1.TabIndex = 4;
-            this.button1.Text = "Select from Stocks-In";
+            this.button1.Text = "Stocks-In Report for Print";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -82,7 +83,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(231, 34);
             this.button2.TabIndex = 5;
-            this.button2.Text = "Select from Stocks-Out";
+            this.button2.Text = "Stocks-Out Report for Print";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -120,15 +121,25 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(158, 34);
             this.button5.TabIndex = 9;
-            this.button5.Text = "Custom Search";
+            this.button5.Text = "Custom Report (Stock-In)";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(474, 137);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(158, 34);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Custom Report (Stock-Out)";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // StockReportsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(665, 283);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.pictureBox1);
@@ -163,5 +174,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
