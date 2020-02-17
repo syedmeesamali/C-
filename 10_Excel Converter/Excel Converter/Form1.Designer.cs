f84d583a04383766616d885fccfbd651ea277aa0
syedmeesamali <syedmeesamali@yahoo.com>
2020-02-17 16:46:09 +0400

    Creating xls to xlsx converter
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

---
+++ 10_Excel Converter/Excel Converter/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Excel_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnImport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(34, 37);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(152, 30);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import xls files";
+            this.btnImport.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 337);
+            this.Controls.Add(this.btnImport);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(681, 376);
+            this.MinimumSize = new System.Drawing.Size(681, 376);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Convert xls to xlsx files";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+    }
+}
+
