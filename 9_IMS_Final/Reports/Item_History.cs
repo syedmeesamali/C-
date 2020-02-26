c1245648b7905aacd16e941662c992054cb44839
syedmeesamali <syedmeesamali@yahoo.com>
2020-02-26 17:20:14 +0400

    Excellent reporting working perfectly now - one product sumamry and history reports working perfectly now
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 9_IMS_Final/Reports/Item_History.cs
+++ 9_IMS_Final/Reports/Item_History.cs
@@ -25,28 +25,22 @@ namespace IMS_Final.Reports
             SqlConnection conn = new SqlConnection(IMS_Final.Properties.Settings.Default.StocksDBConnectionString);
             conn.Open();
             string selectedItem = cboHistory.GetItemText(cboHistory.SelectedItem);
-            adapt = new SqlDataAdapter("select sin.Prod_ID, sin.Prod_Name, format(sum(sin.Cost),'N2') as [Total Cost],  " +
-                    "pr.Re_Order, sin.Expiry, sum(sin.Units) as [Units Bought], " +
-                    " sum(sout.Pcs) as [Units Sold],  " +
-                    "(sum(sin.Units) - sum(sout.Pcs)) as [Stock now] " +
-                    "from StockinTable as sin " +
-                    "inner join StockoutTable as sout " +
-                    "on sout.Prod_Name = sin.Prod_Name " +
-                    "inner join Products as pr " +
-                    "on pr.Prod_Name = sin.Prod_Name " +
-                    "where sin.Prod_Name = '" + selectedItem + "' " +
-                    "group by sin.Prod_ID, sin.Prod_Name, pr.Re_Order, sin.Expiry ",conn);
+            adapt = new SqlDataAdapter("SELECT DISTINCT sin.Prod_ID, sin.Prod_Name, sin.Bought, ISNULL(sout.SOLD,0) as [Sold], " +
+                    "ISNULL((sin.Bought - sout.SOLD),0) AS [Stock Now] " +
+                    "FROM (SELECT Prod_ID, Prod_Name, ISNULL(SUM(Units),0) AS Bought " +
+                    "FROM StockinTable sout GROUP BY Prod_ID, Prod_Name) sin " +
+                    "LEFT JOIN  (SELECT Prod_ID, ISNULL(SUM(Boxes),0) AS SOLD " +
+                    "FROM StockoutTable GROUP BY Prod_ID) sout " +
+                    "ON sin.Prod_ID = sout.Prod_ID " +
+                    "WHERE sin.Prod_Name = '" + selectedItem + "' ",conn);
             dt = new DataTable();
             adapt.Fill(dt);
             dataGridView1.DataSource = dt;
             dataGridView1.Columns[0].Width = 50;
             dataGridView1.Columns[1].Width = 220;
-            dataGridView1.Columns[2].Width = 70;
+            dataGridView1.Columns[2].Width = 90;
             dataGridView1.Columns[3].Width = 90;
             dataGridView1.Columns[4].Width = 90;
-            dataGridView1.Columns[5].Width = 70;
-            dataGridView1.Columns[6].Width = 80;
-            dataGridView1.Columns[7].Width = 70;
             conn.Close();
         }
 
@@ -55,29 +49,22 @@ namespace IMS_Final.Reports
             SqlConnection conn = new SqlConnection(IMS_Final.Properties.Settings.Default.StocksDBConnectionString);
             conn.Open();
             string selectedItem = cboHistory.GetItemText(cboHistory.SelectedItem);
-            adapt = new SqlDataAdapter("select sin.Prod_ID, sin.Prod_Name, format(sin.Cost,'N2') as [Cost], " +
-                    "pr.Re_Order, sin.Expiry, sin.Sup_Name, sin.Units as [Units In], sin.Date as [Date Bought], " +
-                    "sout.Pcs as [Sold], sout.Invoice as [Invoice Ref], sout.Date as [Date Sold], sout.Cust_Name " +
-                    "from StockinTable as sin " +
-                    "inner join StockoutTable as sout " +
-                    "on sout.Prod_Name = sin.Prod_Name " +
-                    "inner join Products as pr on pr.Prod_Name = sin.Prod_Name " +
-                    "where sin.Prod_Name = '" + selectedItem + "' " , conn);
+            adapt = new SqlDataAdapter("SELECT sin.Prod_ID AS [Prod ID], sin.Prod_Name AS [Prod Name], sin.Date, " +
+                "sin.Sup_Name AS[Supplier / Customer], sin.Units AS[Units], FORMAT(sin.Cost, 'N2') AS[Cost / Price], " +
+                "CONVERT(varchar(100),sin.Expiry, 102) AS [Expiry / Invoice] " +
+                "FROM StockinTable AS sin WHERE sin.Prod_Name ='" + selectedItem + "' " +
+                "UNION ALL SELECT sout.Prod_ID, sout.Prod_Name, sout.Date, sout.Cust_Name, sout.Boxes, " +
+                "FORMAT(sout.Price,'N2'), sout.Invoice FROM StockoutTable AS sout WHERE sout.Prod_Name = '" + selectedItem + "' " , conn);
             dt = new DataTable();
             adapt.Fill(dt);
             dataGridView1.DataSource = dt;
-            dataGridView1.Columns[0].Width = 50;
+            dataGridView1.Columns[0].Width = 80;
             dataGridView1.Columns[1].Width = 200;
-            dataGridView1.Columns[2].Width = 50;
-            dataGridView1.Columns[3].Width = 60;
-            dataGridView1.Columns[4].Width = 70;
-            dataGridView1.Columns[5].Width = 140;
-            dataGridView1.Columns[6].Width = 50;
-            dataGridView1.Columns[7].Width = 80;
-            dataGridView1.Columns[8].Width = 50;
-            dataGridView1.Columns[9].Width = 80;
-            dataGridView1.Columns[10].Width = 80;
-            dataGridView1.Columns[11].Width = 210;
+            dataGridView1.Columns[2].Width = 80;
+            dataGridView1.Columns[3].Width = 250;
+            dataGridView1.Columns[4].Width = 90;
+            dataGridView1.Columns[5].Width = 90;
+            dataGridView1.Columns[6].Width = 120;
             conn.Close();
         }
 
@@ -88,14 +75,12 @@ namespace IMS_Final.Reports
                 SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\StocksDB.mdf;Integrated Security=True");
                 conn.Open();
                 //adapt = new SqlDataAdapter("SELECT * FROM StockInTable WHERE Prod_Name like '" + txtSearch.Text + "%'", conn);
-                adapt = new SqlDataAdapter("select sin.Prod_ID, sin.Prod_Name, format(sin.Cost,'N2') as [Cost], " +
-                    "pr.Re_Order, sin.Expiry, sin.Sup_Name, sin.Units as [Units In], sin.Date as [Date Bought], " +
-                    "sout.Pcs as [Sold], sout.Invoice as [Invoice Ref], sout.Date as [Date Sold], sout.Cust_Name " +
-                    "from StockinTable as sin " +
-                    "inner join StockoutTable as sout " +
-                    "on sout.Prod_Name = sin.Prod_Name " +
-                    "inner join Products as pr on pr.Prod_Name = sin.Prod_Name " +
-                    "where sin.Prod_Name like '" + txtSearch.Text + "%'", conn);
+                adapt = new SqlDataAdapter("SELECT sin.Prod_ID AS [Prod ID], sin.Prod_Name AS [Prod Name], sin.Date, " +
+                "sin.Sup_Name AS[Supplier / Customer], sin.Units AS[Units], FORMAT(sin.Cost, 'N2') AS[Cost / Price], " +
+                "CONVERT(varchar(100),sin.Expiry, 102) AS [Expiry / Invoice] " +
+                "FROM StockinTable AS sin WHERE sin.Prod_Name = '" + txtSearch.Text + "%'" +
+                "UNION ALL SELECT sout.Prod_ID, sout.Prod_Name, sout.Date, sout.Cust_Name, sout.Boxes, " +
+                "FORMAT(sout.Price,'N2'), sout.Invoice FROM StockoutTable AS sout WHERE sout.Prod_Name = '" + txtSearch.Text + "%'", conn);
                 dt = new DataTable();
                 adapt.Fill(dt);
                 dataGridView1.DataSource = dt;
