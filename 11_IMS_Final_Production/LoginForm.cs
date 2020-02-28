01086a9be3399fdcb6a2a0f2a5ac6bb9a30803d7
syedmeesamali <syedmeesamali@yahoo.com>
2020-02-28 23:37:51 +0400

    Login form added for main software

---
+++ 11_IMS_Final_Production/LoginForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMS_Final
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            frmMain frm = new frmMain();
+            this.Hide();
+            frm.Show();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
