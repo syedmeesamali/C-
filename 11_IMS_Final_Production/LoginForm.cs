d04e2b3b1e6686c2a0c418c7949fd4648391c6e3
syedmeesamali <syedmeesamali@yahoo.com>
2020-02-29 06:50:16 +0400

    Username password application

--- 11_IMS_Final_Production/LoginForm.cs
+++ 11_IMS_Final_Production/LoginForm.cs
@@ -17,11 +17,36 @@ namespace IMS_Final
             InitializeComponent();
         }
 
+        private bool IsValid()
+        {
+            if (txtUserName.Text.TrimStart() == string.Empty | txtUserName.Text.Length < 5)
+            {
+                MessageBox.Show("User name is required! Length >= 5", "Error");
+                return false;
+            }
+
+            if (txtPassword.Text.TrimStart() == string.Empty | txtPassword.Text.Length < 8)
+            {
+                MessageBox.Show("Password is required! Length >= 8", "Error");
+                return false;
+            }
+
+            return true;
+        }
+
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            frmMain frm = new frmMain();
-            this.Hide();
-            frm.Show();
+            
+            if (IsValid())
+            {
+                frmMain frm = new frmMain();
+                this.Hide();
+                frm.Show();
+            } else
+            {
+                MessageBox.Show("Check login info !");
+            }
+            
         }
 
         private void btnExit_Click(object sender, EventArgs e)
