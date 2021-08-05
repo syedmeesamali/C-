70c668e7eddaa996f1fc34c0c571345953a2df37
syedmeesamali <syedmeesamali@yahoo.com>
2021-08-05 22:02:01 +0400

    Some review of projects

--- 9_IMS_Final_No_Dapper/Properties/AssemblyInfo.cs
+++ 9_IMS_Final_No_Dapper/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2e98377d-610b-4639-a0a9-339851a2e28a")]
