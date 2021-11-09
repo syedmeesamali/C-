1b37f890d87577c1e4e573cc166a34ede0fc4fad
syedmeesamali <syedmeesamali@yahoo.com>
2021-11-09 23:18:29 +0400

    GUI made in C# windows works perfectly well on RPI 3B+ via MONO - Great cross platform working via Csharp

---
+++ 10_RPI/RPI_GUI_02/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RPI_GUI_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Click Done!", "Yes");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world from RPI via C# on Desktop and run via Mono!", "Yes");
+        }
+    }
+}
