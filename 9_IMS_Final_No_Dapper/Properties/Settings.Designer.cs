ec4004b2e27d27e99a4fd42cf47fddc8acc9a319
syedmeesamali <syedmeesamali@yahoo.com>
2023-03-03 10:13:00 +0400

    MDF files updated

--- 9_IMS_Final_No_Dapper/Properties/Settings.Designer.cs
+++ 9_IMS_Final_No_Dapper/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace IMS_Final.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -27,7 +27,7 @@ namespace IMS_Final.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\StocksDB.mdf;" +
-            "Integrated Security=True")]
+            "Integrated Security=True;Connect Timeout=30")]
         public string StocksDBConnectionString {
             get {
                 return ((string)(this["StocksDBConnectionString"]));
